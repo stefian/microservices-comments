1d666c28205ba49d1c73d797bbbc611217f07165
stefian <stefan.ianta@w3ai.net>
2020-06-02 09:06:13 -0300

    added comments list and create routes and listening to 4001

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ const { randomBytes } = require('cryptp');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/posts/:id/comments', (req, res) => {
+
+});
+
+app.post('/posts/:id/coments', (req, res) => {
+
+});
+
+app.listen(4001, () => {
+    console.log('Listening on 4001');
+});
