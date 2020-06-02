430cb34c7bc417eadcdf9a7fe3bb794403c46ac2
stefian <stefan.ianta@w3ai.net>
2020-06-02 10:28:31 -0300

    fixed require crypto for randomBytes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { randomBytes } = require('cryptp');
+const { randomBytes } = require('crypto');
 
 const app = express();
 app.use(bodyParser.json());
