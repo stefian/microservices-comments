17bf7da2f4d01f962ffe41a60ce0e72238b6921e
stefian <stefan.ianta@w3ai.net>
2020-06-02 09:42:54 -0300

    added the logic for creating / post a comment

--- index.js
+++ index.js
@@ -5,12 +5,23 @@ const { randomBytes } = require('cryptp');
 const app = express();
 app.use(bodyParser.json());
 
+const commentsByPostId = {};
+
 app.get('/posts/:id/comments', (req, res) => {
 
 });
 
 app.post('/posts/:id/coments', (req, res) => {
+    const commentId = randomBytes(4).toString(hex);
+    const { content } = req.body;
 
+    const comments = commentsByPostId[req.params.id] || [];   // will give either an array or undefined; if undefine = []
+
+    comments.push({ id: commentId, content });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(comments);
 });
 
 app.listen(4001, () => {
