abc3d38e3415ec5aa25057afe275210646bcf8a5
stefian <stefan.ianta@w3ai.net>
2020-06-04 14:50:16 -0300

    added axios post to event bus /4005 CommentCreated message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,7 +14,7 @@ app.get('/posts/:id/comments', (req, res) => {
     res.send(commentsByPostId[req.params.id] || []);    // sending an array empty or with the comments
 });
 
-app.post('/posts/:id/comments', (req, res) => {
+app.post('/posts/:id/comments', async (req, res) => {
     const commentId = randomBytes(4).toString('hex');
     const { content } = req.body;
 
@@ -21,7 +22,16 @@ app.post('/posts/:id/comments', (req, res) => {
 
     comments.push({ id: commentId, content });
 
-    commentsByPostId[req.params.id] = comments;
+    commentsByPostId[req.params.id] = comments; // saving in commentsByPostId object
+
+    await axios.post('http://localhost:4005/events', {
+        type: 'CommentCreated',
+        data: {
+            id: commentId,
+            content,
+            postId: req.params.id
+        }
+    });
 
     res.status(201).send(comments);
 });
