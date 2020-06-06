c6dda8da718b8653fb7d12d5c4683ae550664764
stefian <stefan.ianta@w3ai.net>
2020-06-06 06:38:53 -0300

    added status: pending on comment creation /push

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];   // will give either an array or undefined; if undefine = []
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: 'pending' });
 
     commentsByPostId[req.params.id] = comments; // saving in commentsByPostId object
 
