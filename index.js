a74b11ea30c41d57b79091bbf1025a0eeec4e7c8
stefian <stefan.ianta@w3ai.net>
2020-06-10 16:16:03 -0300

    updated localhost to internal k8s cluster-ip route

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.post('/posts/:id/comments', async (req, res) => {
 
     commentsByPostId[req.params.id] = comments; // saving in commentsByPostId object
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'CommentCreated',
         data: {
             id: commentId,
@@ -51,7 +51,7 @@ app.post('/events', async (req, res) => {
         })
         comment.status = status; // update comment status with the status from event (CommentModerated)
 
-        await axios.post('http://localhost:4005/events', {
+        await axios.post('http://event-bus-srv:4005/events', {
             type: 'CommentUpdated',
             data: {
                 id,
